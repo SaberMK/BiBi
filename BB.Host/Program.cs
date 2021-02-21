47c78854291af0478a83f63accfcff3d6107fed2
Dmitry Borisov <borisov.dmitry.itp@gmail.com>
2021-02-21 21:13:26 +0300

    initial commit

---
+++ BB.Host/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BB.Host
+{
+    class Program
+    {
+        private const string Prompt = "bb> ";
+
+        static void Main(string[] args)
+        {
+            var line = string.Empty;
+
+            // repl
+            do
+            {
+                Console.Write(Prompt);
+                line = Console.ReadLine();
+                // tokenize string
+                // and parse
+
+
+
+            } while (!string.IsNullOrWhiteSpace(line));
+        }
+    }
+}
