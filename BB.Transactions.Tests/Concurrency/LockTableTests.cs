5a4513fa420e264c700515257f4c44d8b67b496f
Dmitry Borisov <pnegebodyguard@gmail.com>
2021-03-29 23:14:53 +0300

    lock table tests prep

---
+++ BB.Transactions.Tests/Concurrency/LockTableTests.cs
@@ -0,0 +1,30 @@
+﻿using BB.IO;
+using BB.IO.Abstract;
+using BB.Memory.Abstract;
+using BB.Memory.Logger;
+using NUnit.Framework;
+using System;
+
+namespace BB.Transactions.Tests.Concurrency
+{
+    public class LockTableTests
+    {
+        private ILogManager _logManager;
+        private IFileManager _fileManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileManager = new FileManager("temp", "DBs", 100);
+            _logManager = new LogManager(_fileManager, RandomFilename);
+        }
+
+        [Test]
+        public void CanCreateRecoveryManager()
+        {
+
+        }
+
+        private string RandomFilename => $"{Guid.NewGuid()}.bin";
+    }
+}
