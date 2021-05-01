d9ee46456f513e14be15bf2602e2031fea7818ec
Dmitry Borisov <pnegebodyguard@gmail.com>
2021-05-01 23:42:04 +0300

    scan and updatescan interfaces

---
+++ BB.Query/Abstract/IScan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BB.Query.Abstract
+{
+    public interface IScan
+    {
+        void BeforeFirst();
+        void Next();
+        void Close();
+        bool HasField(string fieldName);
+
+
+        Constant GetValue(string fieldName);
+        int GetInt(string fieldName);
+        int GetBool(string fieldName);
+        int GetByte(string fieldName);
+        int GetBlob(string fieldName);
+        int GetString(string fieldName);
+        int GetDate(string fieldName);
+    }
+}
---
+++ BB.Query/Abstract/IUpdateScan.cs
@@ -0,0 +1,23 @@
+﻿using BB.Record.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BB.Query.Abstract
+{
+    public interface IUpdateScan : IScan
+    {
+        void SetValue(string fieldName, Constant constant);
+        void SetInt(string fieldName);
+        void SetBool(string fieldName);
+        void SetByte(string fieldName);
+        void SetBlob(string fieldName);
+        void SetString(string fieldName);
+        void SetDate(string fieldName);
+
+        RID GetRID { get; }
+        void MoveToRID(RID rid);
+    }
+}
