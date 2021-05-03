008ea2ffab933876bbb56ad2ab6bf104972b3928
Dmitry Borisov <pnegebodyguard@gmail.com>
2021-05-03 13:42:56 +0300

    IPlan

---
+++ BB.Query/Abstract/IPlan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BB.Query.Abstract
+{
+    public interface IPlan
+    {
+        int DistinctValues(string fieldName);
+    }
+}
